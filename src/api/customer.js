6137c65f55dfbdd9498589f2709c6d73d23de964
Romic <280385556@qq.com>
2025-01-08 17:17:29 +0800

    s

--- src/api/customer.js
+++ src/api/customer.js
@@ -17,3 +17,35 @@ export function createCustomer(data) {
     data
   })
 } 
+
+// 更新客户
+
+export function updateCustomer(id, data) {
+
+  return request({
+
+    url: `/customers/${id}`,
+
+    method: 'put',
+
+    data
+
+  })
+
+}
+
+
+
+// 删除客户
+
+export function deleteCustomer(id) {
+
+  return request({
+
+    url: `/customers/${id}`,
+
+    method: 'delete'
+
+  })
+
+}
